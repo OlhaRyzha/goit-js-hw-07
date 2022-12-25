af38881171b49ede32a16bae493a38290a1c7f46
OlgaRyzha <orizha11@gmail.com>
2022-12-26 01:14:58 +0200

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,45 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const cardsMarkup = CreateImgCardsMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', cardsMarkup)
+
+
+function CreateImgCardsMarkup(cards){
+return cards.map(({preview, original, description}) => 
+
+  `<div class="gallery__item">
+  <a class="gallery__link"  href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+).join('');
+
+}
+
+galleryEl.addEventListener('click', onLinkClick)
+function onLinkClick(event){
+    event.preventDefault();
+    if(event.target.nodeName !== 'IMG'){
+      return
+    }
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+instance.show();
+
+galleryEl.addEventListener('keydown', e => {
+  if(e.code === 'Escape'){
+    instance.close();
+  }
+} )
+
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+// Change code below this line
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const cardsMarkup = CreateImgCardsMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', cardsMarkup)
+
+function CreateImgCardsMarkup(cards){
+return cards.map(({preview, original, description}) => 
+
+  `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+</a>`
+).join('');
+
+}
+galleryEl.addEventListener('click', onLinkClick)
+function onLinkClick(event){
+    event.preventDefault();
+    if(event.target.nodeName !== 'IMG'){
+      return
+    }
+
+}
+var lightbox = new SimpleLightbox('.gallery a', {});
+lightbox.captionDelay = 250;
+lightbox.captionPosition = 'bottom';
+
